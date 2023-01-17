eea71743f4857064790e0a3471db9d323a83fa10
Lab314 <Lab314@314-CS01>
2023-01-17 14:32:19 +0200

    Add project files.

---
+++ ConsoleApp28OBJECTS/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace ConsoleApp28OBJECTS
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        point p = new point(1, 1, "A");
+         point p2 = new point(2, 2, "B");
+         Console.WriteLine(p);
+         Console.WriteLine(p2);
+            Console.WriteLine(p.Nigga());
+            Console.WriteLine(p2.Nigga());
+
+        }
+    }
+}
---
+++ ConsoleApp28OBJECTS/point.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp28OBJECTS
+{
+    public class point
+    {
+        private int x; private int y; private string name;
+        public point(int xCoor, int yCoor, string nammme)
+        {
+            x = xCoor;
+            y = yCoor;
+            name = nammme;
+        }
+        public int Getx() // getter
+        {
+            return x;
+        }
+
+        public int Gety()
+        {
+            return y;
+        }
+        public string GetName()
+        {
+            return name;
+        }
+        public void SetX(int value)
+        {
+            if (x <= 100)
+            {
+                x = value;
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"{name}({x},{y})";
+        }
+
+        public int Nigga()
+        {
+         if (x > 0 && y > 0)
+                return 1;
+
+            else if (x < 0 && y > 0)
+                return 2;
+
+            else if(x < 0 && y < 0)
+                return 3;
+
+            else if (x > 0 && y < 0)
+                return 4;
+
+            else return -1;
+        }
+    }
+}
