8c7ee6e2c91af28bccc78b06a7b521735c14dec5
Lab314 <Lab314@314-CS01>
2023-01-30 14:26:22 +0200

    asdsdasdasdasdasd

--- ConsoleApp28OBJECTS/Sprite.cs
+++ ConsoleApp28OBJECTS/Sprite.cs
@@ -9,14 +9,14 @@ namespace ConsoleApp28OBJECTS
    
     internal class Sprite: Point
     {
-        public Sprite(int x, int y)
+        public Sprite(int x, int y): base(x, y)
         {
-            base(x,y);
         }
         private override void Draw()
         {
             Console.SetCursorPosition(x, y);
             Console.WriteLine("x");
+            int nigga;
         }
     }
 }
